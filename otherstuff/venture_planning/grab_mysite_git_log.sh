914a460af77e767fccc5a9cce8d74b76758e4511
TL <whymranderson@gmail.com>
2017-08-22 12:13:55 +0800

    Tex_venture add git log. Start tracking compile_ hub.bat and the shell files. Tracking .sty and some flowchart tex files.

---
+++ otherstuff/venture_planning/grab_mysite_git_log.sh
@@ -0,0 +1,17 @@
+cd 'C:\Documents and Settings\The One\My Documents\tony\doni\mysite'
+
+pwd
+
+sleep 5
+
+git log -15 --format=format:"%w(90,0,5)%ad  %B" --date=short > 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\otherstuff\venture_planning\mysite.log'
+
+echo -----SUBMODULE LOG----- >> 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\otherstuff\venture_planning\mysite.log'
+
+cd 'C:\Documents and Settings\The One\My Documents\tony\doni\mysite\db_operations'
+
+pwd
+
+sleep 5
+
+git log -15 --format=format:"%w(90,0,5)%ad  %B" --date=short >> 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\otherstuff\venture_planning\mysite.log'
---
+++ otherstuff/venture_planning/grab_xelatex_git_log.sh
@@ -0,0 +1,7 @@
+cd 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder'
+
+pwd
+
+sleep 5
+
+git log -15 --format=format:"%w(57,0,5)%ad  %B" --date=short > 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\otherstuff\venture_planning\xelatex.log'
