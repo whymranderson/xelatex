16794a4a85023eda3456faced65d4ca1f1a3646b
TL <whymranderson@gmail.com>
2017-09-14 17:29:17 +0800

    restored tetra_premise_4.py to the original one.

--- pgf related/tetra_premise_4.py
+++ pgf related/tetra_premise_4.py
@@ -15,62 +15,77 @@ import matplotlib.colors as colors
 import vector_drawing_basic_geometry_3D
 
 #%% Plotting begins
-# first set viewing angle in 3D space
-elevate = 45
-azimuthal = 0
-# plot generation
 fig2 = pyplot.figure(2,figsize=(4, 4),dpi=100)
 ax2 = p3.Axes3D(fig2)
-ax2.view_init(elev=elevate, azim=azimuthal)
+ax2.view_init(elev=31, azim=-36)
 ax2.set_color_cycle('b')
 
 
-pA = np.array([0,0,0])
-pB = np.array([1,0,0])
-pC = np.array([0,1,0])
-pD = np.array([0,0,1])
+pB = np.array([0,0,0])
+pD = np.array([0,6,0])
+pA = np.array([0.1,1,3])
+pC = np.array([4,2.5,0])
 
 lineAC, = ax2.plot(*zip(pA,pC),linewidth = 2,color='b')
 lineAB, = ax2.plot(*zip(pA,pB),linewidth = 2,color='b')
 lineAD, = ax2.plot(*zip(pA,pD),linewidth = 2,color='b')
-#lineCB, = ax2.plot(*zip(pC,pB),linewidth = 2,color='b')
-#lineCD, = ax2.plot(*zip(pC,pD),linewidth = 2,color='b')
-#lineBD, = ax2.plot(*zip(pB,pD),linewidth = 2,color='b')
+lineCB, = ax2.plot(*zip(pC,pB),linewidth = 2,color='b')
+lineCD, = ax2.plot(*zip(pC,pD),linewidth = 2,color='b')
+lineBD, = ax2.plot(*zip(pB,pD),linewidth = 2,color='b')
 
-#pE = (pA + pD)/2
-#pF = (2*pB + pD)/3
-#pG = (2*pB+3*pC)/5
-#pH = (4*pC+pA)/5
-#pPhantom = pC+(pC-pD)
+pE = (pA + pD)/2
+pF = (2*pB + pD)/3
+pG = (2*pB+3*pC)/5
+pH = (4*pC+pA)/5
+pPhantom = pC+(pC-pD)
 
-#lineCPhan, = ax2.plot(*zip(pC,pPhantom),linewidth = 1,color='b')
-#lineEG, = ax2.plot(*zip(pE,pG),linewidth = 1,color='b')
-#lineHF, = ax2.plot(*zip(pH,pF),linewidth = 1,color='b')
+lineCPhan, = ax2.plot(*zip(pC,pPhantom),linewidth = 1,color='b')
+lineEG, = ax2.plot(*zip(pE,pG),linewidth = 1,color='b')
+lineHF, = ax2.plot(*zip(pH,pF),linewidth = 1,color='b')
 
-#pP = pH + 0.7*(pH-pE)
-#pPprime = pG+1.5*(pG-pF)
-# 
-#linePE, = ax2.plot(*zip(pP,pE),linewidth = 1,color='b')
-#lineFPprime, = ax2.plot(*zip(pF,pPprime),linewidth = 1,color='b')
+pP = pH + 0.7*(pH-pE)
+pPprime = pG+1.5*(pG-pF)
+ 
+linePE, = ax2.plot(*zip(pP,pE),linewidth = 1,color='b')
+lineFPprime, = ax2.plot(*zip(pF,pPprime),linewidth = 1,color='b')
 
 
     
 ax2.text(*pA, s = r'$A$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
-ax2.text(*pB, s = r'$x$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pB, s = r'$B$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pC, s = r'$C$', fontsize=12,verticalalignment='top', horizontalalignment='left')
+ax2.text(*pD, s = r"$D$", fontsize=12,verticalalignment='bottom', horizontalalignment='left')
+ax2.text(*pE, s = r"$E$", fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pF, s = r"$F$", fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pG, s = r"$G$", fontsize=12,verticalalignment='top', horizontalalignment='right')
+ax2.text(*pH, s = r"$H$", fontsize=12,verticalalignment='bottom', horizontalalignment='left')
+ax2.text(*pP, s = r"$P$", fontsize=12,verticalalignment='bottom', horizontalalignment='left')
+ax2.text(*pPprime, s = r"$P'$", fontsize=12,verticalalignment='top', horizontalalignment='left')
 
 #ax2.scatter3D(*zip(pK,pJ,pH))
 
+
+
+
 #draw_perpendicular_sign(np.cross(pB-pQ,pE-pQ),pB-pQ,pE-pQ,pQ,ax2)
 
-# correcting bug of unequal aspect ratio
+
+#axis1.Axis(ax2,'r')
+#ax2.autoscale_view()
+#ax2.pbaspect= [1,1,0.5]
+#ax2.auto_scale_xyz()
+
 Xt,Yt,Zt = zip(pA,pB,pC,pD)
 X = np.array(Xt)
 Y = np.array(Yt)
 Z = np.array(Zt)
+
 max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max() / 2.6
+
+
 mid_x = (X.max()+X.min()) * 0.5
-mid_y = (Y.max()+Y.min()) * 0.5 
-mid_z = (Z.max()+Z.min()) * 0.5
+mid_y = (Y.max()+Y.min()) * 0.5 - 1
+mid_z = (Z.max()+Z.min()) * 0.5 - 1.5
 ax2.set_xlim3d(mid_x - max_range, mid_x + max_range)
 ax2.set_ylim3d(mid_y - max_range, mid_y + max_range)
 ax2.set_zlim3d(mid_z - max_range, mid_z + max_range)
