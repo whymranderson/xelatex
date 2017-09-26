5c990d3816557a2c3eca6c7b0ac2cbd06e4caeef
TL <whymranderson@gmail.com>
2017-09-26 12:47:27 +0800

    created unfinisehd_tasks flowchart. website planning flowchart add procedure to export django-served-graphics -included html pages.

--- otherstuff/venture_planning/compile_hub.bat
+++ otherstuff/venture_planning/compile_hub.bat
@@ -2,8 +2,9 @@
 
 cd C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\otherstuff\venture_planning
 
-set /p id="Choose chapter: (8)all_venture_directions (7)website_planning (6)baking (5)connectors (4)CSGO_arrangement (3)develope_process_v2 (2) fixing (1)tex_venture "
+set /p id="Choose chapter: (9)unfinished_tasks (8)all_venture_directions (7)website_planning (6)baking (5)connectors (4)CSGO_arrangement (3)develope_process_v2 (2) fixing (1)tex_venture "
 
+IF %id% == 9 SET chapname=unfinished_tasks
 IF %id% == 8 SET chapname=all_venture_directions
 IF %id% == 7 SET chapname=website_planning
 IF %id% == 6 SET chapname=baking
