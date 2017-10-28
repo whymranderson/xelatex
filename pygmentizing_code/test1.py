8bba5c540c62f951baf579325ffe92d28303c3f1
TL <whymranderson@gmail.com>
2017-10-28 14:56:01 +0800

    Abandom minted "cusp" code and now use Pygments to generate a verbatim environment and then input it into note. Pygments program is test1.py in pygmentizing_code folder.
    Still need to include the cuspGLstyle.sty in
    whole articale preamble adjust.
    
    Tried changing the verbatim envr to listings envr
    , to see if long line breaks/wraps, with the listings
    verbatim interface in fancyverb package, doestn't
    work. Have not tried spverbatim.

--- pygmentizing_code/test1.py
+++ pygmentizing_code/test1.py
@@ -9,18 +9,33 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import LatexFormatter
 from pygments.formatters import HtmlFormatter
 
+
+
 import sys
 sys.path.insert(0, '../../../Scripts/cordtrans/Physics_and_Animation_Modules_Library')
+sys.path.insert(0, '../../../Scripts/cordtrans/Demo_Examples/spinning_top_gyroscope/four_classical_motions_demo/OpenGL_animation')
 
-fh = file('RBPlotFunc.html','w')
-fl = file('RBPlotFunc.tex','w')
+#fh = file('RBPlotFunc.html','w')
+#fl = file('RBPlotFunc.tex','w')
 
-RBplot_f = file("../../../Scripts/cordtrans/Physics_and_Animation_Modules_Library/RBPlotFunctionV5.py")
+fh = file('cusp_GL.html','w')
+fl = file('cusp_GL.tex','w')
 
-code_list = RBplot_f.readlines()
+
+pyfile = file("../../../Scripts/cordtrans/Demo_Examples/spinning_top_gyroscope/four_classical_motions_demo/OpenGL_animation/cusp_GL.py")
+
+code_list = pyfile.readlines()
 code = "".join(code_list)
 highlight(code,PythonLexer(),HtmlFormatter(),outfile=fh)
-highlight(code,PythonLexer(),LatexFormatter(full = True),outfile=fl)
+#highlight(code,PythonLexer(),LatexFormatter(full = True, docclass = 'standalone'),outfile=fl)
+highlight(code,PythonLexer(),LatexFormatter(verboptions = 'fontsize=\\small, numbers = left, frame=bottomline'),outfile=fl)
+
+# save style definitions
+stylestr = LatexFormatter().get_style_defs()
+#print stylestr
+fs = file('cuspGLstyle.sty','w')
+fs.write(stylestr)
 
 fh.close()
 fl.close()
+fs.close()
