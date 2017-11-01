5732428430b3de4f2ff3cd0693eeda908be364fd
TL <whymranderson@gmail.com>
2017-11-01 13:58:04 +0800

    Bat add production choice. Orien chapter delect c code. Major paragraphs rearrangement in production chapter. Pygments doc_style.sty add cuspGLstyle definition. rV14 include production chapter.

--- Chapter2Texlive.bat
+++ Chapter2Texlive.bat
@@ -2,12 +2,13 @@
 
 cd C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\
 
-set /p id="Choose chapter: (1)Program (2)Case_Study (3)Orientation Estimation (4)accuracy  "
+set /p id="Choose chapter: (1)Program (2)Case_Study (3)Orientation Estimation (4)accuracy  (5)production"
 
 IF %id% == 1 SET chapname=Program
 IF %id% == 2 SET chapname=Case_Study
 IF %id% == 3 SET chapname=Orientation_Estimation
 IF %id% == 4 SET chapname=accuracy
+IF %id% == 5 SET chapname=production
 
 python Encode_Preamble_Adjust_for_chapter.py %chapname%
 
