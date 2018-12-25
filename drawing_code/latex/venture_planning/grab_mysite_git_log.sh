0e4667474463f5d2d60bc46d74b4809516177331
TL <whymranderson@gmail.com>
2018-12-25 11:41:08 +0800

    (DONE) GO arrangement venture content can be moved to the blogpost. fixed git_log grab shell. saw log output but no verbatim output in pdf. Error says box too high?

--- drawing_code/latex/venture_planning/grab_mysite_git_log.sh
+++ drawing_code/latex/venture_planning/grab_mysite_git_log.sh
@@ -1,17 +1,17 @@
-cd 'C:\Documents and Settings\The One\My Documents\tony\doni\mysite'
+cd 'C:\Users\user\Desktop\myblog'
 
 pwd
 
 sleep 5
 
-git log -30 --format=format:"%w(90,0,5)%ad  %B" --date=short > 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\drawing_code\latex\venture_planning\mysite.log'
+git log -30 --format=format:"%w(90,0,5)%ad  %B" --date=short > 'C:\Users\user\Desktop\xelatex_Transend\drawing_code\latex\venture_planning\mysite.log'
 
-echo -----SUBMODULE LOG----- >> 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\drawing_code\latex\venture_planning\mysite.log'
+echo -----SUBMODULE LOG----- >> 'C:\Users\user\Desktop\xelatex_Transend\drawing_code\latex\venture_planning\mysite.log'
 
-cd 'C:\Documents and Settings\The One\My Documents\tony\doni\mysite\db_operations'
+cd 'C:\Users\user\Desktop\db_op'
 
 pwd
 
 sleep 5
 
-git log -30 --format=format:"%w(90,0,5)%ad  %B" --date=short >> 'C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\drawing_code\latex\venture_planning\mysite.log'
+git log -30 --format=format:"%w(90,0,5)%ad  %B" --date=short >> 'C:\Users\user\Desktop\xelatex_Transend\drawing_code\latex\venture_planning\mysite.log'
