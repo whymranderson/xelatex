777e55aab55090896820e75b3c2453b76108b885
TL <whymranderson@gmail.com>
2019-01-01 13:32:51 +0800

    math_contest folder moved to onedrive. Deleted some absolete folders.

--- math_contest_examsheet_w_answers/run_python.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Documents and Settings\The One\My Documents\tony\2014\xelatexfolder\advanced_math"
-python.exe ureplace_plus_preamble_adjust_AdMath.py
---
+++ piano_scores/lilypond files/oak/make.bat
@@ -0,0 +1,7 @@
+cd "C:\Documents and Settings\The One\My Documents\tony\Scripts\lilypond files"
+
+lilypond oak.ly
+
+oak.pdf
+
+pause
