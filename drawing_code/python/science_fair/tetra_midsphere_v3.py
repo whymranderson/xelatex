09cbefce21ecd4690cdf667e5f078864e390d480
TL <whymranderson@gmail.com>
2019-03-07 15:27:23 +0800

    checking out fourwayvalve tetra_v3.

--- drawing_code/python/science_fair/tetra_midsphere_v3.py
+++ drawing_code/python/science_fair/tetra_midsphere_v3.py
@@ -30,13 +30,14 @@ from annotate_program import circle_full
 from annotate_program import third_seg_incircled
 from annotate_program import rotation_matrix
 from annotate_program import draw_xyz_coordinate_unit_vectors
-from annotate_program import cylinder
+from annotate_program import draw_perpendicular_sign
+from annotate_program import solve_tetra_insphere
 
 #### The plotting of a vector-based graphics using the above points location information.
-fig2 = pyplot.figure(2,figsize=(6, 6),dpi=100)
+fig2 = pyplot.figure(2,figsize=(4, 4),dpi=100)
 ax2 = p3.Axes3D(fig2)
 #ax2.view_init(elev=10, azim=187)
-ax2.view_init(elev=10, azim=-125)
+ax2.view_init(elev=36, azim=66)
 ax2.set_color_cycle('b')
 
 '''
@@ -51,134 +52,91 @@ linez.set_linewidth(1)
 linez.set_color('k')
 ax2.text(0,0,6, r'$z_s$', fontsize=18,verticalalignment='bottom', horizontalalignment='left')
 '''
-
+pA = np.array([-7,-6,-1])
 pC = np.array([0,6,0])
 pB = np.array([1,3.5,7])
 pD = np.array([7,1.5,0])
 
-#step 1, determine a sphere that insuscribed BCD
-incenterBCD,inradiusBCD,normvecBCD, pE, pF, pN= incircle3D(pB,pC,pD)
-pO = incenterBCD + 1.2*inradiusBCD*normvecBCD
-sphereR = np.sqrt(np.square(1.2*inradiusBCD)+np.square(inradiusBCD))
+#step 1, determine vector BO
+#first determine a segment length BN, or fix a N
+len_a = 3
+pN = pB + len_a*(pD-pB)/np.linalg.norm(pD-pB)
+# this will fix E and H
+pH = pB + len_a*(pA-pB)/np.linalg.norm(pA-pB)
+pE = pB + len_a*(pC-pB)/np.linalg.norm(pC-pB)
+#to find Htemp1,2, first find incenter
+midHN = (pN + pH)/2
+n_BmidHN = (midHN-pB)/np.linalg.norm(midHN-pB)
+amp_BIabd=np.linalg.norm(pN-pB) * np.linalg.norm(pB-pN)/np.linalg.norm(midHN-pB)
+incenterABD = pB + amp_BIabd*n_BmidHN
+midHE = (pE + pH)/2
+n_BmidHE = (midHE-pB)/np.linalg.norm(midHE-pB)
+amp_BIabc=np.linalg.norm(pE-pB) * np.linalg.norm(pB-pE)/np.linalg.norm(midHE-pB)
+incenterABC = pB + amp_BIabc*n_BmidHE
+midNE = (pE + pN)/2
+n_BmidNE = (midNE-pB)/np.linalg.norm(midNE-pB)
+amp_BIbcd=np.linalg.norm(pE-pB) * np.linalg.norm(pB-pE)/np.linalg.norm(midNE-pB)
+incenterBCD = pB + amp_BIbcd*n_BmidNE
+# to find O, first find Htemp
+r1 = incenterABD-pH
+r1n = r1/np.linalg.norm(r1)
+r2 = incenterABC-pH
+r2n = r2/np.linalg.norm(r2)
+cos_alpha = np.dot(r1n,r2n)
+pHtemp1 = pH + (np.linalg.norm(r1)/cos_alpha)*r2n
+pHtemp2 = pH + (np.linalg.norm(r2)/cos_alpha)*r1n
+pO =return_intersection_under_Ceva_Theorem(pHtemp2,pHtemp1,pH,incenterABC,incenterABD)
 
-#step 2, establish an somewhat arbitrary second incircle, with same tangent point N
-#tempM = rotmat_from_A_2_B(incenterBCD-pO,pN-pO)
-#from incenterABD find pH and pG
-angle = 69
-tempM = rotation_matrix(pD-pN,np.radians(angle))
-direction = np.dot(tempM,(pN-pO))*np.cos(np.radians(angle))
-incenterABD = pO+direction#/np.linalg.norm(direction)*np.linalg.norm(incenterBCD-pO)+ pO
-M4pH = rotmat_from_A_2_B(pN-incenterABD,pB-incenterABD)
-pH = incenterABD + np.dot(M4pH,pB-incenterABD)/np.linalg.norm(pB-incenterABD)*np.linalg.norm(pN-incenterABD)
-M4pG = rotmat_from_A_2_B(pN-pD,incenterABD-pD)
-pG = pD + np.dot(M4pG,incenterABD-pD)/np.linalg.norm(pD-incenterABD)*np.linalg.norm(pN-pD)
-##calculate third seg using x,y,z,r relation on wiki (Chu's resulte)
-x,y,r = np.linalg.norm(pB-pH),np.linalg.norm(pN-pD),np.linalg.norm(pN-incenterABD)
-z = third_seg_incircled(x,y,r)
-pA = (pH-pB)/np.linalg.norm(pH-pB)*z + pH 
-# realized AC will go through sphere. So there are restrictions here. Maybe sphereR has to be smaller than
-# both incircle radius? or the angle between two triangles has to be greater than sphereR. Or combined.
-# Should comeback and examine later. Restiction is four triangles formed by tangent points obtuse.
+#graph step 1
+lineIabdO = ax2.plot(*zip(incenterABD,pO),linewidth = 2,color='b')
+lineIabcO = ax2.plot(*zip(incenterABC,pO),linewidth = 2,color='b')
+lineBC = ax2.plot(*zip(pB,pC),linewidth = 2,color='b')
+lineBA = ax2.plot(*zip(pB,pA),linewidth = 2,color='b')
+lineBD = ax2.plot(*zip(pB,pD),linewidth = 2,color='b')
 
-#from pE pH find inplane tangent circle
-M90 = rotation_matrix(np.cross(pB-pC,pB-pA),np.pi/2)
-cHE_pH = np.dot(M90,pB-pH)
-n_cHE_pH = cHE_pH/np.linalg.norm(cHE_pH)
-bb = np.linalg.norm(pB-pH)
-aa = np.linalg.norm(pE-pH)/2
-r_cirHE = bb/np.sqrt(np.square(bb/aa)-1)
-centerHE = n_cHE_pH * r_cirHE + pH 
-
-#find point M
-pM = return_third_point_on_a_triagle_under_Ceva_Theorem(pA,pC,pB,pE,pH)
-incenterACD,inradiusACD,normvecACD, pp, ppp, pppp= incircle3D(pA,pC,pD)
-
-#lineAC, = ax2.plot(*zip(pA,pC),linewidth = 2,color='b')
-#lineAB, = ax2.plot(*zip(pA,pB),linewidth = 2,color='b')
-#lineAD, = ax2.plot(*zip(pA,pD),linewidth = 2,color='b')
-
-# graph step one
-lineCB, = ax2.plot(*zip(pC,pB),linewidth = 2,color='b')
-lineCD, = ax2.plot(*zip(pC,pD),linewidth = 2,color='b')
-lineBD, = ax2.plot(*zip(pB,pD),linewidth = 2,color='b')
-#lineIbcdO, = ax2.plot(*zip(pO,incenterBCD),linewidth = 1,color='b',linestyle=':')
-incircleBCD = circle_full(normvecBCD,
-                          (-incenterBCD+pB),
-                            inradiusBCD,40) + incenterBCD
-ax2.plot(*np.transpose(incircleBCD),linewidth=1,linestyle=':')
-plot_front(ax2,pO[0],pO[1],pO[2],sphereR)
-plot_back(ax2,pO[0],pO[1],pO[2],sphereR)
+ax2.text(*pH, s = r'$H$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pO, s = r'$O$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
 ax2.text(*pB, s = r'$B$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
-ax2.text(*pC, s = r'$C$', fontsize=12,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pD, s = r"$D$", fontsize=12,verticalalignment='top', horizontalalignment='left')
-ax2.text(*pN, s = r"$N$", fontsize=12,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pE, s = r"$E$", fontsize=12,verticalalignment='bottom', horizontalalignment='right')
-ax2.text(*pF, s = r"$F$", fontsize=12,verticalalignment='top', horizontalalignment='right')
-#ax2.text(*incenterBCD, s = r"$I_{BCD}$", fontsize=12,verticalalignment='bottom', horizontalalignment='left')
-ax2.text(*pO, s = r"$O$", fontsize=12,verticalalignment='top', horizontalalignment='left')
-
-# graph step 2
+ax2.text(*pC, s = r'$C$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pD, s = r'$D$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pN, s = r'$N$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*pE, s = r'$E$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+ax2.text(*incenterABC, s = r'$I_{abc}$', fontsize=12,verticalalignment='bottom', horizontalalignment='left')
+ax2.text(*incenterABD, s = r'$I_{abd}$', fontsize=12,verticalalignment='bottom', horizontalalignment='right')
+draw_perpendicular_sign(np.cross(r1,r2), -r2, pHtemp2-incenterABC, incenterABC, ax2, 0.2)
+draw_perpendicular_sign(np.cross(r1,r2),pHtemp1-incenterABD, -r1, incenterABD, ax2, 0.2)
+#ax2.scatter3D(*zip(pJ,pK,pL,pI,pO,pM,pN,pH,pG,pE,pF))
 incircleABD = circle_full(incenterABD-pO, pN-incenterABD, np.linalg.norm(pN-incenterABD), 30) + incenterABD
 ax2.plot(*np.transpose(incircleABD),linewidth=1,linestyle=':')
-#lineIabdO, = ax2.plot(*zip(pO,incenterABD),linewidth = 1,color='b',linestyle=':')
-lineAB, = ax2.plot(*zip(pA,pB),linewidth = 2,color='b')
-lineAD, = ax2.plot(*zip(pA,pD),linewidth = 2,color='b')
-ax2.text(*pA, s = r'$A$', fontsize=12,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pH, s = r"$H$", fontsize=12,verticalalignment='bottom', horizontalalignment='left')
-ax2.text(*pG, s = r"$G$", fontsize=12,verticalalignment='top', horizontalalignment='left')
-#ax2.text(*incenterABD, s = r"$I_{ABD}$", fontsize=12,verticalalignment='top', horizontalalignment='left')
+incircleABC = circle_full(incenterABC-pO, pH-incenterABC, np.linalg.norm(pH-incenterABC), 30) + incenterABC
+ax2.plot(*np.transpose(incircleABC),linewidth=1,linestyle=':')
+incircleBCD = circle_full(incenterBCD-pO, pN-incenterBCD, np.linalg.norm(pN-incenterBCD), 30) + incenterBCD
+ax2.plot(*np.transpose(incircleBCD),linewidth=1,linestyle=':')
 
-# graph step 3
-circleHE = circle_full(np.cross(pB-pC,pB-pA), pH-centerHE, r_cirHE, 30) + centerHE
-ax2.plot(*np.transpose(circleHE),linewidth=1,linestyle=':',color='r')
-lineAC, = ax2.plot(*zip(pA,pC),linewidth = 2,color='b')
+r1 = np.linalg.norm(pH-incenterABD)
+r2 = np.linalg.norm(pH-incenterABC)
+r3 = np.linalg.norm(pN-incenterBCD)
+c = np.linalg.norm(pH-pB)
 
-# graph step 4
-ax2.text(*pM, s = r"$M$", fontsize=12,verticalalignment='top', horizontalalignment='right')
-incenterACD,inradiusACD,normvecACD, pp, ppp, pppp= incircle3D(pA,pC,pD)
+a_roots = solve_tetra_insphere(r1,r3,r2,c)
+#print a_roots
+a = 8.74796477
+b = (r1**2)*(a+c)/(a*c-(r1**2))
+d = (r2**2)*(a+c)/(a*c-(r2**2))
+r4 = np.sqrt(d*a*b/(d+a+b))
+
+pDD = pN + b * (pN-pB)/np.linalg.norm(pN-pB)
+pAA = pH + a * (pH-pB)/np.linalg.norm(pH-pB)
+pCC = pE + d * (pE-pB)/np.linalg.norm(pE-pB)
+lineAADD = ax2.plot(*zip(pAA,pDD),linewidth = 2,color='b')
+lineAACC = ax2.plot(*zip(pAA,pCC),linewidth = 2,color='b')
+lineDDCC = ax2.plot(*zip(pDD,pCC),linewidth = 2,color='b')
+
+print pB,pAA,pDD,pCC
+
+incenterACD, _, _,pF,_,_ = incircle3D(pDD,pCC,pAA) 
 incircleACD = circle_full(incenterACD-pO, pF-incenterACD, np.linalg.norm(pF-incenterACD), 30) + incenterACD
 ax2.plot(*np.transpose(incircleACD),linewidth=1,linestyle=':')
-#ax2.text(*incenterACD, s = r"$I_{ACD}$", fontsize=12,verticalalignment='bottom', horizontalalignment='right')
-#ax2.text(*centerHE, s = r"$I_{ABC}$", fontsize=12,verticalalignment='top', horizontalalignment='left')
-#lineIacdO, = ax2.plot(*zip(pO,incenterACD),linewidth = 1,color='b',linestyle=':')
-#lineIabcO, = ax2.plot(*zip(pO,centerHE),linewidth = 1,color='b',linestyle=':')
-
-# return all ceva points and connect their lines and plot Gergonne point
-pK = return_intersection_under_Ceva_Theorem(pA,pD,pB,pN,pH)
-pI = return_intersection_under_Ceva_Theorem(pA,pB,pC,pE,pM)
-pL = return_intersection_under_Ceva_Theorem(pA,pD,pC,pF,pM)
-pJ = return_intersection_under_Ceva_Theorem(pB,pD,pC,pF,pE)
-pGP = return_intersection_under_Ceva_Theorem(pB,pA,pF,pL,pJ)
-
-ax2.text(*pK, s = r'$K$',   fontsize=10,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pI, s = r'$I$',   fontsize=10,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pL, s = r'$L$',   fontsize=10,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pJ, s = r'$J$',   fontsize=10,verticalalignment='top', horizontalalignment='right')
-ax2.text(*pGP, s = r'$GP$', fontsize=10,verticalalignment='top', horizontalalignment='right')
-
-ax2.scatter3D(*zip(pE,pF,pG,pH,pN,pM),c = ['blue'])
-ax2.scatter3D(*zip(pO),c = ['red'])
-ax2.scatter3D(*zip(pK,pI,pL,pJ),c = ['green'])
-ax2.scatter3D(*zip(pGP),c = ['green'],s = [18])
-
-lineAN, = ax2.plot(*zip(pA,pN),linewidth = 1,color='g')
-lineGB, = ax2.plot(*zip(pG,pB),linewidth = 1,color='g')
-lineDH, = ax2.plot(*zip(pD,pH),linewidth = 1,color='g')
-lineAE, = ax2.plot(*zip(pA,pE),linewidth = 1,color='g')
-lineMB, = ax2.plot(*zip(pM,pB),linewidth = 1,color='g')
-lineCH, = ax2.plot(*zip(pC,pH),linewidth = 1,color='g')
-lineCN, = ax2.plot(*zip(pC,pN),linewidth = 1,color='g')
-lineFB, = ax2.plot(*zip(pF,pB),linewidth = 1,color='g')
-lineDE, = ax2.plot(*zip(pD,pE),linewidth = 1,color='g')
-lineAF, = ax2.plot(*zip(pA,pF),linewidth = 1,color='g')
-lineCG, = ax2.plot(*zip(pC,pG),linewidth = 1,color='g')
-lineDM, = ax2.plot(*zip(pD,pM),linewidth = 1,color='g')
-
-lineAJ, = ax2.plot(*zip(pA,pJ),linewidth = 1,color='y')
-lineBL, = ax2.plot(*zip(pB,pL),linewidth = 1,color='y')
-lineCK, = ax2.plot(*zip(pC,pK),linewidth = 1,color='y')
-lineDI, = ax2.plot(*zip(pD,pI),linewidth = 1,color='y')
-cylinder(ax2)
 # Add transparent faces
 #vt1 = [pA,pB,pD]
 #tr1 = p3.art3d.Poly3DCollection([vt1],color = 'r', alpha=0.3)
@@ -190,6 +148,7 @@ cylinder(ax2)
 
 
 Xt,Yt,Zt = zip(pO,pA,pB,pC,pD)
+#Xt,Yt,Zt = zip(pH,pHtemp1,pHtemp2,pB)
 X = np.array(Xt)
 Y = np.array(Yt)
 Z = np.array(Zt)
@@ -199,7 +158,7 @@ max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max() 
 
 mid_x = (X.max()+X.min()) * 0.5
 mid_y = (Y.max()+Y.min()) * 0.5 + 0.6
-mid_z = (Z.max()+Z.min()) * 0.5 - 0.8
+mid_z = (Z.max()+Z.min()) * 0.5 - 0.6
 ax2.set_xlim3d(mid_x - max_range, mid_x + max_range)
 ax2.set_ylim3d(mid_y - max_range, mid_y + max_range)
 ax2.set_zlim3d(mid_z - max_range, mid_z + max_range)
@@ -212,7 +171,7 @@ ax2.w_xaxis.line.set_visible(False) #turn off axis visibility
 ax2.w_yaxis.line.set_color([0,0,0,0]) # change the color of axis
 ax2.w_zaxis.line.set_color([0,0,0,0])
 ax2.set_axis_off()  #-> this can turn off the background curtain
-#pyplot.savefig('./pgf_files/tetra_midsphere_Gergonne.pgf')
+#pyplot.savefig('./pgf_files/tetra_midsphere.pgf')
 
 pyplot.show()
 
