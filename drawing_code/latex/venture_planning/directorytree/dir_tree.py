3b54cfcde67190bf1189965e10e70e87e2ccf59d
TL <whymranderson@gmail.com>
2020-02-21 23:15:56 +0800

    dir tree

--- drawing_code/latex/venture_planning/directorytree/dir_tree.py
+++ drawing_code/latex/venture_planning/directorytree/dir_tree.py
@@ -12,13 +12,15 @@ def list_files(startpath):
 def list_files_menukeys(startpath):
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
-        indent = '\>' * (level)
-        print('{}{}{}/{}\\\\'.format(indent,r'\nixpath{', os.path.basename(root),'}'))
-        subindent = '\>' * (level + 1)
-        for f in files:
-            print('{}{}{}{}\\\\'.format(subindent,r'\nixfile{',f,'}'))
+        if level < 3:
+            indent = '\>' * (level)
+            print('{}{}{}/{}\\\\'.format(indent,r'\nixpath{', os.path.basename(root),'}'))
+            subindent = '\>' * (level + 1)
+            for f in files:
+                print('{}{}{}{}\\\\'.format(subindent,r'\nixfile{',f,'}'))
 
-listpath = r"C:\Documents and Settings\The One\My Documents\tony\Scripts\GyroSoft Simulation\1_Demo_Examples"
+#listpath = r"C:\Documents and Settings\The One\My Documents\tony\Scripts\GyroSoft Simulation\1_Demo_Examples"
+listpath = r"C:\texlive\2018"
 
 #list_files(listpath)
 list_files_menukeys(listpath)
